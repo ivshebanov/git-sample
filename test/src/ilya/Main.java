a82ccd1fade217124894baf540480e55bb877e27
Ilya Shebanov <IVShebanov@gmail.com>
2017-08-09 17:25:33 +0300

    тесты Лямбды

--- test/src/ilya/Main.java
+++ test/src/ilya/Main.java
@@ -1,8 +1,29 @@
 package ilya;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("Маша", 19));
+        list.add(new Person("Мария", 20));
+        list.add(new Person("Настя", 21));
+        list.add(new Person("Марина", 22));
+
+        for (Person l : list) {
+            System.out.println(l.getName() + " " + l.getAge());
+        }
+        System.out.println();
+        list.forEach(System.out::println);
+        System.out.println();
+        list.stream().filter(p -> p.getAge() > 20).forEach(System.out::println);
+        System.out.println();
+        list.stream().filter(p -> p.getAge() > 20).sorted((p1, p2) -> p1.getName().compareTo(p2.getName())).forEach(System.out::println);
+        System.out.println();
+        double d = list.stream().filter(p -> p.getAge() > 20).sorted(Comparator.comparing(Person::getName)).mapToInt(Person::getAge).average().getAsDouble();
+        System.out.println(d);
     }
 }
