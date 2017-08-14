1830e84d19ae29339570a1269822354d10caffb8
Ilya Shebanov <IVShebanov@gmail.com>
2017-08-14 15:54:37 +0300

    BubbleSort_v2

--- BubleSort/src/ilya/Main.java
+++ BubleSort/src/ilya/Main.java
@@ -6,12 +6,14 @@ public class Main {
 
     public static void main(String[] args) {
         int[] newMas = {4, 3, 6, 8, 2, 9, 12, 3, 5};
-        bubleSotrt(newMas);
+//        bubleSotrt(newMas);
+        bubbleSortWhile(newMas);
+
     }
 
     private static void bubleSotrt(int[] arr) {
-        int leng = arr.length;
-        for (int i = leng - 1; i >= 0; i--) {
+        int leng = arr.length - 1;
+        for (int i = leng; i >= 0; i--) {
             for (int j = 0; j < i; j++) {
                 if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
@@ -20,6 +22,23 @@ public class Main {
                 }
             }
         }
+        System.out.println(Arrays.toString(arr));
+    }
+
+    private static void bubbleSortWhile(int[] arr) {
+        int count;
+        int leng = arr.length;
+        do {
+            count = 0;
+            for (int i = 0; i < leng - 1; i++) {
+                if (arr[i] > arr[i + 1]) {
+                    int temp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = temp;
+                    count++;
+                }
+            }
+        } while (count > 0);
         System.out.println(Arrays.toString(arr));
     }
 }
