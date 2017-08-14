dbc8260d835853937aa6b4ce0f365991ef2a7d69
Ilya Shebanov <IVShebanov@gmail.com>
2017-08-14 13:33:47 +0300

    bubleSort-1

--- BubleSort/src/ilya/Main.java
+++ BubleSort/src/ilya/Main.java
@@ -1,8 +1,25 @@
 package ilya;
 
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
-        
+        int[] newMas = {4, 3, 6, 8, 2, 9, 12, 3, 5};
+        bubleSotrt(newMas);
+    }
+
+    private static void bubleSotrt(int[] arr) {
+        int leng = arr.length;
+        for (int i = leng - 1; i >= 0; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
