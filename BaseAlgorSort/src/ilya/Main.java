f1791caeb77107d181c01184f885fe03ca5a1d12
Ilya Shebanov <IVShebanov@gmail.com>
2017-08-15 13:20:08 +0300

    пузырек, вставка, выборка

--- BaseAlgorSort/src/ilya/Main.java
+++ BaseAlgorSort/src/ilya/Main.java
@@ -2,13 +2,23 @@ package ilya;
 
 import java.util.Arrays;
 
-public class Main {
+public class Main {0
 
     public static void main(String[] args) {
         int[] arr = {3, 2, 5, 1, 6, 7, 4, 9, 8, 0};
+        int[] array = {0, 1, 2, 3, 4, 5, 7, 6, 9, 8};
 
         System.out.println(Arrays.toString(bubbleSortI(arr)));
+        System.out.println(Arrays.toString(bubbleSortI(array)));
+
         System.out.println(Arrays.toString(bubbleSortII(arr)));
+        System.out.println(Arrays.toString(bubbleSortII(array)));
+
+        System.out.println(Arrays.toString(sampleSort(arr)));
+        System.out.println(Arrays.toString(sampleSort(array)));
+
+        System.out.println(Arrays.toString(insertSort(arr)));
+        System.out.println(Arrays.toString(insertSort(array)));
     }
 
 
@@ -48,6 +58,44 @@ public class Main {
         } while (check > 0);
         long stop = System.nanoTime();
         System.out.println("bubbleSortII " + (stop - start));
+        return array;
+    }
+
+    private static int[] sampleSort(int[] arr) {
+        int[] array = Arrays.copyOf(arr, arr.length);
+        long start = System.nanoTime();
+        int leng = array.length;
+        for (int i = 0; i < leng; i++) {
+            for (int j = i; j < leng - 1; j++) {
+                if (array[i] > array[j + 1]) {
+                    int temp = array[i];
+                    array[i] = array[j + 1];
+                    array[j + 1] = temp;
+                }
+            }
+        }
+        long stop = System.nanoTime();
+        System.out.println("sampleSort " + (stop - start));
+        return array;
+    }
+
+    private static int[] insertSort(int[] arr) {
+        int[] array = Arrays.copyOf(arr, arr.length);
+        long start = System.nanoTime();
+        int leng = array.length;
+        int element;
+        int indexElement;
+        for (int insert = 1; insert < leng; insert++) {
+            element = array[insert];
+            indexElement = insert;
+            while (indexElement > 0 && array[indexElement] < array[indexElement - 1]) {
+                array[indexElement] = array[indexElement - 1];
+                indexElement--;
+                array[indexElement] = element;
+            }
+        }
+        long stop = System.nanoTime();
+        System.out.println("insertSort " + (stop - start));
         return array;
     }
 }
