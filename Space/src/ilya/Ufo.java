c4dabfc4b99db27faf74d962f02d17b438aee7df
Ilya Shebanov <IVShebanov@gmail.com>
2018-01-20 14:41:56 +0300

    Ufo

--- Space/src/ilya/Ufo.java
+++ Space/src/ilya/Ufo.java
@@ -2,7 +2,38 @@ package ilya;
 
 public class Ufo extends BaseObject {
 
-    public Ufo(double x, double y, double radius) {
-        super(x, y, radius);
+    private static int[][] matrix = {
+            {0, 0, 0, 0, 0},
+            {0, 0, 1, 0, 0},
+            {1, 1, 1, 1, 1},
+            {0, 1, 1, 1, 0},
+            {0, 0, 0, 0, 0},
+    };
+
+    public Ufo(double x, double y) {
+        super(x, y, 3);
+    }
+
+    @Override
+    public void move() {
+        double dx = Math.random() * 2 - 1;
+         double dy = Math.random() * 2 - 1;
+        x += dx;
+        y += dy;
+
+        checkBorders(radius, Space.game.getWidth() - radius + 1, radius - 1, Space.game.getHeight() / 2 - 1);
+
+        int ramdom = (int) (Math.random() * 10);
+        if (ramdom == 10) {
+            fire();
+        }
+    }
+
+    public void draw(Canvas canvas) {
+        canvas.drowMatrix(x - radius + 1, y - radius + 1, matrix, 'U');
+    }
+
+    public void fire() {
+        Space.game.getBombs().add(new Bomb(x, y + 3));
     }
 }
